38df806845ffe557e6754f1d6475112c65cfafc5
naraeingowtham17 <ng8382@srmist.edu.in>
2026-04-25 18:13:03 +0530

    UC5: Implement enhanced for loop for greeting multiple names

--- HelloApp.java
+++ HelloApp.java
@@ -1,16 +1,16 @@
-
 public class HelloApp {
 
     public static void main(String[] args) {
 
-        String name;
-
-        if (args.length > 0) {
-            name = String.join(" ", args);
-        } else {
-            name = "World";
+        // If no arguments → default greeting
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } 
+        else {
+            // Enhanced for loop (for-each)
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
+            }
         }
-
-        System.out.println("Hello " + name + "!");
     }
 }
